b7827e5d6ba531ac3afe2debd604e82455b2f44d
Abdulaziz3404 <abdulazizturgunaliyev3404@gmail.com>
2022-10-30 11:57:30 -0700

    UI dizayn bo'ldi

--- app/src/main/java/com/example/ussdmobile/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/ussdmobile/adapters/MainViewPagerAdapter.kt
@@ -1,37 +1,7 @@
 package com.example.ussdmobile.adapters
 
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.ussdmobile.MainFragment
-import com.example.ussdmobile.fragments.*
+class MainViewPagerAdapter {
 
-class MainViewPagerAdapter(var itemList: List<String>, fragmentActivity: FragmentActivity) :
-    FragmentStateAdapter(fragmentActivity) {
 
-    override fun getItemCount(): Int {
-        return itemList.size
-    }
 
-    override fun createFragment(position: Int): Fragment {
-
-        var fragment = Fragment()
-
-        when (position) {
-            0 -> {
-                fragment = BeelineFragment()
-            }
-            1 -> {
-                fragment = UzmobileFragment()
-            }
-            2 -> {
-                fragment = MobiuzFragment()
-            }
-            3 -> {
-                fragment = UcellFragment()
-            }
-        }
-
-        return fragment
-    }
 }
